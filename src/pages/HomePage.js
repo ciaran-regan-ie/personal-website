c0d37f091747d5c102db9931d7afa903616882c2
ciaran-regan-ie <ciaranregan123@gmail.com>
2025-08-09 08:54:03 +0900

    add phd

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,18 +17,23 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
   return (
     <Container>
       <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ my: 2, mt: 4 }}>
-            <Typography variant="h4">Ciaran Regan</Typography>
-            <IconButton onClick={onToggleDarkMode}>
-              {darkMode ? <Brightness7 /> : <Brightness4 />}
-            </IconButton>
-          </Box>
+        <Typography variant="h4">Ciaran Regan</Typography>
+        <IconButton onClick={onToggleDarkMode}>
+          {darkMode ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+      </Box>
+
       <Grid container spacing={1} alignItems="center">
         <Grid item xs={12} md={isLgUp ? 8 : 12}>
           <Typography variant="body1" sx={{ my: 1 }}>
             ALife and AI researcher.
           </Typography>
           <Box display="flex" alignItems="center" sx={{ mt: 1 }}>
-            <Link href="https://www.google.com/maps/@36.1103664,140.1011046,3a,75y,135.67h,105.23t/data=!3m6!1e1!3m4!1swG2p1242HKNDpBrypcUcKg!2e0!7i13312!8i6656?entry=ttu" target="_blank" rel="noopener noreferrer">
+            <Link
+              href="https://www.google.com/maps/@36.1103664,140.1011046,3a,75y,135.67h,105.23t/data=!3m6!1e1!3m4!1swG2p1242HKNDpBrypcUcKg!2e0!7i13312!8i6656?entry=ttu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <IconButton><Language /></IconButton>
             </Link>
             <Typography variant="body2">Tsukuba, Japan</Typography>
@@ -45,23 +50,46 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
               <IconButton><GitHub /></IconButton>
             </Link>
             <Link href="https://scholar.google.com/citations?user=q35leGMAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">
-          <IconButton>
-            <GoogleScholarIcon />
-          </IconButton>
-        </Link>
+              <IconButton>
+                <GoogleScholarIcon />
+              </IconButton>
+            </Link>
           </Box>
         </Grid>
+
         {isLgUp && (
           <Grid item lg={4} style={{ paddingLeft: '24px', textAlign: 'right' }}>
             <img src={profilePhoto} alt="Ciaran Regan" style={{ width: '100px', height: 'auto', borderRadius: '8px' }} />
           </Grid>
         )}
       </Grid>
-      
+
       {/* Education Section */}
       <Grid item xs={12} sx={{ mt: 1 }}>
         <Typography variant="h6">Education</Typography>
         <List dense>
+          <ListItem>
+            <ListItemText primary="Ph.D. in Computer Science" secondary={
+              <>
+                <Link 
+                  href="https://www.tsukuba.ac.jp/en/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    color: 'inherit', 
+                    textDecoration: 'none', 
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      textDecorationColor: theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  University of Tsukuba
+                </Link>
+                {" (Ongoing)"}
+              </>
+            } />
+          </ListItem>
           <ListItem>
             <ListItemText primary="M.Sc. in Computer Science" secondary={
               <>
@@ -81,7 +109,8 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
                   University of Tsukuba
                 </Link>
                 {" (2025)"}
-              </>} />
+              </>
+            } />
           </ListItem>
           <ListItem>
             <ListItemText primary="B.A in Theoretical Physics" secondary={
@@ -102,9 +131,9 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
                   Trinity College Dublin
                 </Link>
                 {" (2021)"}
-              </>} />
+              </>
+            } />
           </ListItem>
-          {/* Add more education items here */}
         </List>
       </Grid>
 
@@ -131,8 +160,8 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
                   Sakana AI
                 </Link>
                 {" (2024 - present)"}
-              </>}
-            />
+              </>
+            } />
           </ListItem>
           <ListItem>
             <ListItemText primary="Research Assistant" secondary={
@@ -153,14 +182,14 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
                   Chiba Institute of Technology
                 </Link>
                 {" (2024 - present)"}
-              </>}
-            />
+              </>
+            } />
           </ListItem>
           <ListItem>
-            <ListItemText primary="Research Assistant" secondary={
+            <ListItemText primary="Data Science Analyst" secondary={
               <>
                 <Link 
-                  href="https://blankspace.jp" 
+                  href="https://accenture.com" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   sx={{ 
@@ -172,35 +201,12 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
                     },
                   }}
                 >
-                  Blank Space
+                  Accenture
                 </Link>
-                {" (2023 - present)"}
-              </>}
-            />
+                {" (2021 - 2022)"}
+              </>
+            } />
           </ListItem>
-            <ListItem>
-              <ListItemText primary="Data Science Analyst" secondary={
-                <>
-                  <Link 
-                    href="https://accenture.com" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    sx={{ 
-                      color: 'inherit', 
-                      textDecoration: 'none', 
-                      '&:hover': {
-                        textDecoration: 'underline',
-                        textDecorationColor: theme.palette.text.primary,
-                      },
-                    }}
-                  >
-                    Accenture
-                  </Link>
-                  {" (2021 - 2022)"}
-                </>}
-              />
-            </ListItem>
-          {/* Add more experience items here */}
         </List>
       </Grid>
 
@@ -208,191 +214,96 @@ const HomePage = ({ darkMode, onToggleDarkMode }) => {
       <Grid item xs={12} sx={{ mt: 1 }}>
         <Typography variant="h6">Publications</Typography>
         <List dense>
-        <ListItem>
+          <ListItem>
             <ListItemText 
-            primary={
-              <Link 
-                href="https://arxiv.org/abs/2406.12374" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                sx={{ 
-                  color: 'inherit', 
-                  textDecoration: 'none', 
-                  '&:hover': {
-                    textDecoration: 'underline',
-                    textDecorationColor: theme.palette.text.primary,
-                  },
-                }}
-              >
-                Problem-Solving in Language Model Networks
-              </Link>
+              primary={
+                <Link 
+                  href="https://arxiv.org/abs/2505.05522" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    color: 'inherit', 
+                    textDecoration: 'none', 
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      textDecorationColor: theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  Continuous Thought Machines
+                </Link>
+              } 
+              secondary="In Review (2025)" 
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText 
+              primary={
+                <Link 
+                  href="https://arxiv.org/abs/2406.12374" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    color: 'inherit', 
+                    textDecoration: 'none', 
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      textDecorationColor: theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  Problem-Solving in Language Model Networks
+                </Link>
               } 
               secondary="ALIFE (2024)" 
             />
           </ListItem>
           <ListItem>
             <ListItemText 
-            primary={
-              <Link 
-                href="https://arxiv.org/abs/2406.04663" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                sx={{ 
-                  color: 'inherit', 
-                  textDecoration: 'none', 
-                  '&:hover': {
-                    textDecoration: 'underline',
-                    textDecorationColor: theme.palette.text.primary,
-                  },
-                }}
-              >
-                LLM-POET: Evolving Complex Environments using Large Language Models
-              </Link>
+              primary={
+                <Link 
+                  href="https://arxiv.org/abs/2406.04663" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    color: 'inherit', 
+                    textDecoration: 'none', 
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      textDecorationColor: theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  LLM-POET: Evolving Complex Environments using Large Language Models
+                </Link>
               } 
               secondary="GECCO (2024)" 
             />
           </ListItem>
           <ListItem>
             <ListItemText 
-            primary={
-              <Link 
-                href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0294228" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                sx={{ 
-                  color: 'inherit', 
-                  textDecoration: 'none', 
-                  '&:hover': {
-                    textDecoration: 'underline',
-                    textDecorationColor: theme.palette.text.primary,
-                  },
-                }}
-              >
-                Simulating emergence of novelties using agent-based models
-              </Link>
+              primary={
+                <Link 
+                  href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0294228" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    color: 'inherit', 
+                    textDecoration: 'none', 
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      textDecorationColor: theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  Simulating Emergence of Novelties using Agent-Based Models
+                </Link>
               } 
               secondary="PLOS ONE (2023)" 
             />
           </ListItem>
-          {/* Add more project items here */}
         </List>
       </Grid>
-
-      {/* Talks Section */}
-      <Grid item xs={12} sx={{ mt: 1 }}>
-        <Typography variant="h6">Presentations</Typography>
-        <List dense>
-          <ListItem>
-              <ListItemText primary="InferenceQL Hands-On" secondary={
-                <>
-                  Probabilistic Computing Workshop (2024), Tokyo, Japan
-                </>}
-              />
-            </ListItem>
-          <ListItem>
-              <ListItemText primary="Reinforcement and Novelty in Innovation Networks" secondary={
-                <>
-                  <Link 
-                    href="https://netscix2024.netscisociety.org/" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    sx={{ 
-                      color: 'inherit', 
-                      textDecoration: 'none', 
-                      '&:hover': {
-                        textDecoration: 'underline',
-                        textDecorationColor: theme.palette.text.primary,
-                      },
-                    }}
-                  >
-                    NetSciX (2024), Venice, Italy
-                  </Link>
-                </>}
-              />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="AI + IT = Knowledge Technology" secondary={
-                <>
-                  <Link 
-                    href="https://www.digitaldunlaoghaire.ie/" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    sx={{ 
-                      color: 'inherit', 
-                      textDecoration: 'none', 
-                      '&:hover': {
-                        textDecoration: 'underline',
-                        textDecorationColor: theme.palette.text.primary,
-                      },
-                    }}
-                  >
-                    Digital Dun Laoighare (2024), Dublin, Ireland
-                  </Link>
-                </>}
-              />
-            </ListItem>
-            <ListItem>
-            <ListItemText primary="Towards Scalable Generative Agent Systems" secondary={
-              <>
-                  Probabilistic Computing Workshop (2023), Tokyo, Japan
-              </>}
-            />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Reinforcement and Novelty in Innovation Networks" secondary={
-                <>
-                  <Link 
-                    href="https://www.alife-japan.org/eng-news/the-8th-artificial-life-research-group-workshop-announced" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    sx={{ 
-                      color: 'inherit', 
-                      textDecoration: 'none', 
-                      '&:hover': {
-                        textDecoration: 'underline',
-                        textDecorationColor: theme.palette.text.primary,
-                      },
-                    }}
-                  >
-                    Artificial Life Japan Workshop 8 (2023), Kyoto, Japan
-                  </Link>
-                </>}
-              />
-            </ListItem>
-          {/* Add more project items here */}
-        </List>
-      </Grid>
-
-           {/* Projects Section */}
-           <Grid item xs={12} sx={{ mt: 1 }}>
-        <Typography variant="h6">Projects</Typography>
-        <List dense>
-        <ListItem>
-          <ListItemText 
-          primary={
-            <Link 
-              href="https://graphtft.com" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              sx={{ 
-                color: 'inherit', 
-                textDecoration: 'none', 
-                '&:hover': {
-                  textDecoration: 'underline',
-                  textDecorationColor: theme.palette.text.primary,
-                },
-              }}
-            >
-              GraphTFT
-            </Link>
-            } 
-            secondary="Graph-based app for Teamfight Tactics" 
-          />
-          </ListItem>
-          {/* Add more project items here */}
-        </List>
-      </Grid>
-
     </Container>
   );
 };
